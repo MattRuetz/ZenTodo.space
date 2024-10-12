82acfc2b39a5d9d13e9d28669db6be2989dd03c6
Matt Ruetz <matt.b.ruetz@gmail.com>
2024-10-11 22:43:38 -0700

    getUserIdFromClerk logging

--- src/app/utils/getUserIdFromClerk.ts
+++ src/app/utils/getUserIdFromClerk.ts
@@ -9,6 +9,9 @@ export async function getUserIdFromClerk(
     req: NextRequest
 ): Promise<string | undefined> {
     const { userId: clerkId } = getAuth(req);
+
+    console.log('clerkId', clerkId);
+
     if (!clerkId) {
         return undefined;
     }
@@ -16,13 +19,17 @@ export async function getUserIdFromClerk(
     // Check if userId is cached
     let userId = userCache.get(clerkId);
     if (!userId) {
+        console.log('no user cached');
         const user = await User.findOne({ clerkId });
         if (!user) {
+            console.log('user not found in DB');
             return undefined; // Return null if user is not found
         }
         userId = user._id.toString(); // Convert ObjectId to string
         userCache.set(clerkId, userId as string); // Cache the userId
     }
+
+    console.log('userId', userId);
 
     return userId; // Return the userId
 }
