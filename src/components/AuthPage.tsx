746f1f54e16463404f3ef00e65bcefb4240bb91a
Matt Ruetz <matt.b.ruetz@gmail.com>
2024-10-14 15:22:49 -0700

    img tag

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -31,14 +31,12 @@ const AuthPage: React.FC<AuthPageProps> = ({ action }) => {
             {isMobileSize ? (
                 <div className="min-h-screen flex flex-col bg-black">
                     <div className="p-6 flex flex-col items-center justify-center w-11/12 mx-auto">
-                        <Image
-                            src="/images/ZenTodo_Log_white.png"
+                        <img
+                            src="/images/ZenTodo_Log_white.webp"
                             alt="ZenTodo Logo"
                             width={300}
                             height={83}
                             className="mb-4"
-                            priority={true}
-                            unoptimized={true}
                         />
                         <p className="text-sm text-neutral-content italic text-center mb-6">
                             {quote}
@@ -56,14 +54,12 @@ const AuthPage: React.FC<AuthPageProps> = ({ action }) => {
                         </div>
                     </div>
                     <div className="p-3 w-8/12 h-full flex flex-col items-center justify-center">
-                        <Image
-                            src="/images/ZenTodo_Log_slate900.png"
+                        <img
+                            src="/images/ZenTodo_Log_slate900.webp"
                             alt="ZenTodo Logo"
                             width={600}
                             height={200}
                             className="mb-10"
-                            priority={true}
-                            unoptimized={true}
                         />
                         <p className="mt-2 text-md text-neutral-content italic max-w-md text-center">
                             {quote}
