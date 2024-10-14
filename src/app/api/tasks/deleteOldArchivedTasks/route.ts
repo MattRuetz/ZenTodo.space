4fa5c75dac626d413e6a409e36a15ce662a1fa9b
Matt Ruetz <matt.b.ruetz@gmail.com>
2024-10-14 14:05:42 -0700

    debugging cron

--- src/app/api/tasks/deleteOldArchivedTasks/route.ts
+++ src/app/api/tasks/deleteOldArchivedTasks/route.ts
@@ -1,7 +1,12 @@
 // src/app/api/tasks/deleteOldArchivedTasks/route.ts
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { deleteOldArchivedTasks } from '../deleteOldArchivedTasks';
 
+// export async function GET(req: NextRequest) {
+//     return deleteOldArchivedTasks(req);
+// }
+
 export async function GET(req: NextRequest) {
-    return deleteOldArchivedTasks(req);
+    console.log('Cron job executed at:', new Date().toISOString());
+    return NextResponse.json({ message: 'Cron job executed' }, { status: 200 });
 }
