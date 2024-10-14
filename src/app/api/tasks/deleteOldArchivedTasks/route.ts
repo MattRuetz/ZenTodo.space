611f848bc9b9f29c151ca775aefafdfc7bae63c2
Matt Ruetz <matt.b.ruetz@gmail.com>
2024-10-14 14:24:25 -0700

    updated middleware

--- src/app/api/tasks/deleteOldArchivedTasks/route.ts
+++ src/app/api/tasks/deleteOldArchivedTasks/route.ts
@@ -7,6 +7,12 @@ import { deleteOldArchivedTasks } from '../deleteOldArchivedTasks';
 // }
 
 export async function GET(req: NextRequest) {
-    console.log('Cron job executed at:', new Date().toISOString());
-    return NextResponse.json({ message: 'Cron job executed' }, { status: 200 });
+    try {
+        console.log('CRON job started at:', new Date().toISOString());
+        // Rest of your code...
+        return new NextResponse('CRON job executed', { status: 200 });
+    } catch (error) {
+        console.error('Error in CRON job:', error);
+        return new NextResponse('Error in CRON job', { status: 500 });
+    }
 }
