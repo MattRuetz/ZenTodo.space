cd95d0f3aace803a38860775b3db5118769b526d
Matt Ruetz <matt.b.ruetz@gmail.com>
2024-10-25 15:52:44 -0700

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,26 @@ import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+    title: 'ZenTodo',
+    description: 'Productivity made fun and simple.', // Replace with your app's description
+    openGraph: {
+        title: 'ZenTodo',
+        description: 'Productivity made fun and simple.',
+        url: 'https://www.zentodo.space',
+        siteName: 'ZenTodo',
+        images: [
+            {
+                url: 'https://www.zentodo.space/images/ZenToDo_Log_slate700.png', // Replace with the actual path to your OG image
+                width: 1200,
+                height: 630,
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+};
+
 export default function RootLayout({
     children,
 }: Readonly<{
